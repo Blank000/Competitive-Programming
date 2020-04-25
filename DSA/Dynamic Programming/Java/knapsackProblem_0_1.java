78c6179db5273db30b7c93110312d859633dcb51
Blank000 <pratap.alok.raj@gmail.com>
2020-04-26 03:38:06 +0530

    Knapsack Problem code added for Java and Python both

---
+++ DSA/Dynamic Programming/Java/knapsackProblem_0_1.java
@@ -0,0 +1,13 @@
+package Dynamic_Progamming;
+
+public class knapsackProblem_0_1 {
+    //This Algo is used to find out how can we maxmize the value of some items. Lets say there are few weights of some values and you want to choose items out of it such that the calue is maximised. So we use this.
+
+    //  Imagine the weights are 1,2,3  and the values are 2,4,1. How can we choose two items such that total value is maximum
+    //  We will make an arraylist of weights vs number of different weights. Here we will make 
+    // and then we can see that holding weight 2 and 3 will be a wise option.
+
+    //Method
+
+
+}
